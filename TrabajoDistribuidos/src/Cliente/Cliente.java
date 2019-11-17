2dc18f37d03ce2b9abe021c05cfd19577171e011
Miguel <Miguel@DESKTOP-1O81L9H>
2019-11-17 12:10:27 +0100

    Initial commit

---
+++ TrabajoDistribuidos/src/Cliente/Cliente.java
@@ -0,0 +1,12 @@
+package Cliente;
+
+public class Cliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//aqui se desarrollara el cliente
+	}
+
+}
---
+++ TrabajoDistribuidos/src/Servidor/Servidor.java
@@ -0,0 +1,12 @@
+package Servidor;
+
+public class Servidor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//aqui se desarrollara el servidor
+	}
+
+}
